d9a9135740d7033045408469cb3daa36b58e3609
Jared <you@example.com>
2024-11-05 20:10:24 -0700

    Reverting to Commit Four

---
+++ src/sections/hero.jsx
@@ -0,0 +1,12 @@
+const Hero = () => {
+    return (
+        <section className="min-h-screen w-full flex-col relative">
+            <div className="w-full mx-auto flex flex-col sm:mt36 mt-20 c-space gap-3">
+                <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Hi, I am Jared!<span className="waving-hand">👋</span></p>
+                <p className="hero_tag text-gray_gradient">Building Innovative Pages & Applications</p>
+            </div>
+        </section>
+    );
+}
+
+export default Hero;
