861362eb8ed0afca03bc527aa9f00068fc4a58bf
Jared <you@example.com>
2024-11-05 19:49:38 -0700

    Revert "Fourth Commit - Made Title"
    This reverts commit 85b53c31774a20567625c043b5c2e6789837271e.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Navbar from './sections/navbar.jsx';
-import Hero from './sections/hero.jsx'; // Changed the import path to match the correct file name
 
 const App = () => {
   return (
     <main className="max-w-7xl mx-auto">
       <Navbar />
-      <Hero />
     </main>
   );
 }
